7abfc9b97bcbc9c56880e39c1938075740ce9b8c
Heun11 <pacificko787@gmail.com>
2025-10-18 10:18:59 +0200

    + build

--- source/main.c
+++ source/main.c
@@ -20,11 +20,11 @@ int main(void)
   printf("starting now...\n");
   gpio_put(ONBOARD_LED, 1);
 
-  BOARD_Board board = BOARD_SetupBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq");
+  // BOARD_Board board = BOARD_SetupBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq");
   // BOARD_Board board = BOARD_SetupBoard("k7/6P1/8/8/8/8/8/K7 w");
   // BOARD_Board board = BOARD_SetupBoard("r3k2r/r3P3/6n1/4b3/1K6/8/4p3/8 b kq");
   // BOARD_Board board = BOARD_SetupBoard("2R5/1k6/8/8/8/8/K7/8 w ");
-  // BOARD_Board board = BOARD_SetupBoard("7R/k7/8/8/8/8/K7/8 w ");
+  BOARD_Board board = BOARD_SetupBoard("r2qkb1r/p1ppp1pp/bp5n/1B2np2/3NP3/1P1P4/P1P2PPP/RNBQK2R w ");
       
   BOARD_PrintPrettyBoard(&board.board);
   
